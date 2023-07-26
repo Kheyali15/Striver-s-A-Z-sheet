787636814c1779eeb0d3b4a9adbf104c3ebad2b4
Kheyali15 <kheyalidhara15@gmail.com>
2023-07-26 23:03:07 +0530

    committing string

--- Array/Medium/sortColors.java
+++ Array/Medium/sortColors.java
@@ -1,8 +1,52 @@
 // 75. Sort Colors # https://leetcode.com/problems/sort-colors/
 
 import java.util.*;
-class Solution {
+
+class sortColors {
+
+    public static void sortColors(int[] nums, int length) {
+        int low = 0,
+            high = length - 1,
+            mid = 0,
+            temp;
+        while (mid <= high) {
+            switch(nums[mid]) {
+                case 0:
+                    temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    temp = nums[high];
+                    nums[high] = nums[mid];
+                    nums[mid] = temp;
+                    high--;
+                    break;
+            }
+        }
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array size : ");
+        int length = sc.nextInt();
+        int[] arr = new int[length];
+        System.out.print("Enter numbers : ");
+        for (int i=0 ; i<length ; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sortColors(arr, length);
+
+        System.out.print("Sorted array : ");
+        for (int i=0 ; i<length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
     }
 }
