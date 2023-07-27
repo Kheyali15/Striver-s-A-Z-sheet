7b1675a0321784a4f16c20cee4e60205b2dbe32a
Kheyali15 <kheyalidhara15@gmail.com>
2023-07-27 10:10:17 +0530

    committing binaryOperations

--- String/Easy/CalculateBinaryOperations.java
+++ String/Easy/CalculateBinaryOperations.java
@@ -1,11 +1,49 @@
 import java.util.Scanner;
 
 public class CalculateBinaryOperations {
+
+    public static int calculateBinaryOperations(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+
+        int result = 0;
+        char operator = 'A';
+
+        for (int i=0 ; i<str.length() ; i++) {
+            char ch = str.charAt(i);
+
+            if (operator == 'A' || operator == 'B' || operator == 'C') {
+                operator = ch;
+            } else {
+                int number = Integer.parseInt(Character.toString(ch), 2);
+
+                switch (operator) {
+                    case 'A':
+                        result &= number;
+                        break;
+                    case 'B':
+                        result |= number;
+                        break;
+                    case 'C':
+                        result ^= number;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter String : ");
         String str = sc.nextLine();
-        
+        int result = calculateBinaryOperations(str);
+        System.out.println("Result: " + result);
         sc.close();
     }
 }
